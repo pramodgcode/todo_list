ebd172e8021ddba8aceff764794343e12f425483
pramodcode <pramod.g4451@gmail.com>
2024-06-01 17:07:34 +0530

    s

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 $(()=> {
     $("[type=submit]").click((e)=> {
         e.preventDefault();
-        let username = $("[name=username]").val().trim();
-        let password = $("[name=password]").val().trim();
+        let username = $("[name=username]").val();
+        let password = $("[name=password]").val();
 
         if(username != "admin" && password != "12345"){
             alert("Username / password Error");
